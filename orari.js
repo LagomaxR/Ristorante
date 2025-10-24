10b5b594bf31ecffd72deb5ad1d7122d5bd04118
LagomaxR <lagomaxravenna+ristorante@gmail.com>
2025-10-24 22:45:06 +0200

    Update orari.js

--- orari.js
+++ orari.js
@@ -1,7 +1,8 @@
 lun = 'Chiuso';
 mar = 'Chiuso';
 mer = 'Chiuso';
-gio = '19:00 - 22:30';
-ven = '19:00 - 22:30';
-sab = '19:30 - 22:30';
+gio = 'FERIE';
+ven = 'FERIE';
+sab = '19:00 - 22:30';
+
 dom = '19:00 - 21:30';
